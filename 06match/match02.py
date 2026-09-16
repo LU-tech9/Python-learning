cde722c582b7989069e8000725c88096bfebf965
LU <2061229371@qq.com>
2026-09-16 22:27:56 +0800

    更新代码

---
+++ 06match/match02.py
@@ -0,0 +1,17 @@
+control = input("请输入指令:")
+
+match control:
+    case "上"|"w"|"W":
+        print("角色向上移动")
+    case "下"|"s"|"S":
+        print("角色向下移动")
+    case "左"|"a"|"A":
+        print("角色向左移动")
+    case "右"|"d"|"D":
+        print("角色向右移动")
+    case "跳"|" ":
+        print("角色跳跃")
+    case "攻击"|"j"|"J":
+        print("角色攻击")
+    case "退出"|"esc"|"ESC":
+        print("角色退出游戏")
