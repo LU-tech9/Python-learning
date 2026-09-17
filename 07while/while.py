e9706087c147c1b4b9d9bc00d6f504c839119f29
LU <2061229371@qq.com>
2026-09-17 14:57:33 +0800

    更新代码

---
+++ 07while/while.py
@@ -0,0 +1,14 @@
+i = 0
+while i<10:
+    print("你好")
+    i+=1
+else:
+    print("循环结束")
+i = 0
+total = 0
+while i <= 100:
+    if i % 2 == 0:
+        total += i
+    i += 1
+else:
+    print(total)
